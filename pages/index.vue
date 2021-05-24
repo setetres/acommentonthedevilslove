ebc15ebd41ada1ca32a6eb0efc5e12fb3f85d038
Guilherme Pangnotta <setetres@gmail.com>
2021-05-24 09:21:44 -0300

    WIP

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,21 @@
     <header
       class="home bg-gray-800 flex flex-col flex-1 min-h-screen items-center justify-center overflow-hidden relative"
     >
-      <div class="max-w-2xl space-y-8 z-10">
+      <div class="max-w-2xl space-y-4 z-10">
         <h1 class="font-extrabold italic text-8xl text-white mb-4">
           A comment on the Devil's love
         </h1>
         <div class="space-y-8 font-medium text-white">
-          <p class="text-5xl">
-            These poems are respectfully dedicated to The Cheshire-Cat.
-          </p>
-          <div class="text-2xl space-y-4">
-            <div>
-              <p>A comment on the Devil's love.</p>
-              <p>"Twelve English Poems"</p>
-            </div>
-            <p>Brasília, Summer 1973.</p>
+          <div class="text-5xl">
+            <p>"Twelve English Poems"</p>
           </div>
-          <p class="text-lg">Fausto Alvim Junior</p>
+          <div class="text-2xl space-y-4 max-w-xs">
+            <p>These poems are respectfully dedicated to The Cheshire-Cat.</p>
+          </div>
+          <div class="text-lg">
+            <p>Brasília, Summer 1973.</p>
+            <p>Fausto Alvim Junior</p>
+          </div>
         </div>
       </div>
       <img class="top-0 absolute h-full" src="~/assets/images/home.png" />
